bad81c5d8314470aad764135bbbb2b0a558c71d1
juliotrigueiroo <159463745+juliotrigueiroo@users.noreply.github.com>
2024-04-30 09:44:11 -0300

    Add files via upload

---
+++ java A1.java
@@ -0,0 +1,25 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+     
+    int idade = 18; 
+    
+
+if (idade >= 18) {
+    System.out.println("voce ja pode votar ");
+}
+    else {
+    System.out.println("voce ainda nao pode votar ");}
+
+   
+  }
+}
+
